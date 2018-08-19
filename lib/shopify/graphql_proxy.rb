d92481280ea0735ca4f1ff0048244b24dbdf603c
imontalvomiguel <ubaldo.im@gmail.com>
2018-08-18 19:53:00 -0500

    Add private app support

--- lib/shopify/graphql_proxy.rb
+++ lib/shopify/graphql_proxy.rb
@@ -4,23 +4,33 @@ module Shopify
   class GraphQLProxy < Rack::Proxy
     PROXY_BASE_PATH = "/graphql"
     GRAPHQL_PATH = "/admin/api/graphql.json"
-    VERSION = "0.1.0"
+    VERSION = "0.2.0"
+
+    def initialize(app = nil, opts= {})
+      super
+      @shop = opts[:shop] if opts[:shop]
+      @password = opts[:password] if opts[:password]
+    end
 
     def perform_request(env)
-      request = Rack::Request.new(env)
-      path_info = request.path_info
-      request_method = request.request_method
+      @request = Rack::Request.new(env)
+
+      path_info = @request.path_info
+      request_method = @request.request_method
 
       if path_info =~ %r{^#{PROXY_BASE_PATH}} && request_method == "POST"
-        unless request.session.key?(:shopify)
+        shop = @shop ? @shop : value_from_shopify_session(:shop)
+        token = @password ? @password : value_from_shopify_session(:token)
+
+        unless shop && token
           return ["403", {"Content-Type" => "text/plain"}, ["Unauthorized"]]
         end
 
-        backend = URI("https://#{request.session[:shopify][:shop]}#{GRAPHQL_PATH}")
+        backend = URI("https://#{shop}#{GRAPHQL_PATH}")
 
         env["HTTP_HOST"] = backend.host
         env["PATH_INFO"] = backend.path
-        env["HTTP_X_SHOPIFY_ACCESS_TOKEN"] = request.session[:shopify][:token]
+        env["HTTP_X_SHOPIFY_ACCESS_TOKEN"] = token
         env["SCRIPT_NAME"] = ""
         env["HTTP_COOKIE"] = nil
 
@@ -28,6 +38,12 @@ module Shopify
       else
         @app.call(env)
       end
+    end
+
+    private
+
+    def value_from_shopify_session(key)
+      @request.session.key?(:shopify) ? @request.session[:shopify][key] : nil;
     end
   end
 end
--- test/shopify/graphql_proxy_test.rb
+++ test/shopify/graphql_proxy_test.rb
@@ -23,7 +23,7 @@ class Shopify::GraphQLProxyTest < Minitest::Test
   def test_response_is_403_unauthorized_when_session_does_not_exist
     response = Rack::MockRequest.new(@middleware).post("/graphql")
 
-    assert 402, response.status
+    assert 403, response.status
     assert_equal "Unauthorized", response.body
   end
 
@@ -40,6 +40,27 @@ class Shopify::GraphQLProxyTest < Minitest::Test
     browser = Rack::Test::Session.new(Rack::MockSession.new(@middleware))
     browser.post("/graphql", {}, opts)
 
+    assert_proxy(browser)
+  end
+
+  def test_takes_a_shop_and_password_from_options_and_proxies_the_request
+    @middleware = Shopify::GraphQLProxy.new(@app, {:shop => "snowdevil.myshopify.com", :password => "23"})
+
+    browser = Rack::Test::Session.new(Rack::MockSession.new(@middleware))
+    browser.post("/graphql")
+
+    assert_proxy(browser)
+  end
+
+  def test_responds_403_unauthorized_if_shop_has_not_access_token
+    @middleware = Shopify::GraphQLProxy.new(@app, {:shop => "snowdevil.myshopify.com"})
+    response = Rack::MockRequest.new(@middleware).post("/graphql")
+
+    assert 403, response.status
+    assert_equal "Unauthorized", response.body
+  end
+
+  def assert_proxy(browser)
     assert_equal "snowdevil.myshopify.com", browser.last_request.get_header("HTTP_HOST")
     assert_equal "/admin/api/graphql.json", browser.last_request.get_header("PATH_INFO")
     assert_equal "23", browser.last_request.get_header("HTTP_X_SHOPIFY_ACCESS_TOKEN")
